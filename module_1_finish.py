d8c8b4af5107a9b149ca1542fa189a18f2e24cc3
Руслан <rustikbogdanov19@gmail.com>
2024-09-17 13:25:25 +0300

    17.09

---
+++ module_1_finish.py
@@ -0,0 +1,24 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {1:'Johnny',
+            2:'Bilbo',
+            3:'Steve',
+            4:'Khendrik',
+            5:'Aaron'}
+a=sum(grades[0])
+b=len(grades[0])
+a_1=sum(grades[1])
+b_1=len(grades[1])
+a_2=sum(grades[2])
+b_2=len(grades[2])
+a_3=sum(grades[3])
+b_3=len(grades[3])
+a_4=sum(grades[4])
+b_4=len(grades[4])
+print((a/b),(a_1/b_1),(a_2/b_2),(a_3/b_3),(a_4/b_4))
+my_dict={}
+my_dict['key1']='Aaron'
+my_dict['key2']='Bilbo'
+my_dict['key3']='Johnny'
+my_dict['key4']='Khendrik'
+my_dict['key5']='Steve'
+print(my_dict)
