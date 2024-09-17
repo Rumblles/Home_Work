0c7743937fe5338e3da88d834b7545a684566815
Руслан <rustikbogdanov19@gmail.com>
2024-09-17 13:43:08 +0300

    17.09

--- module_1_finish.py
+++ module_1_finish.py
@@ -1,24 +1,19 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {1:'Johnny',
-            2:'Bilbo',
-            3:'Steve',
-            4:'Khendrik',
-            5:'Aaron'}
-a=sum(grades[0])
-b=len(grades[0])
-a_1=sum(grades[1])
-b_1=len(grades[1])
-a_2=sum(grades[2])
-b_2=len(grades[2])
-a_3=sum(grades[3])
-b_3=len(grades[3])
-a_4=sum(grades[4])
-b_4=len(grades[4])
-print((a/b),(a_1/b_1),(a_2/b_2),(a_3/b_3),(a_4/b_4))
-my_dict={}
-my_dict['key1']='Aaron'
-my_dict['key2']='Bilbo'
-my_dict['key3']='Johnny'
-my_dict['key4']='Khendrik'
-my_dict['key5']='Steve'
-print(my_dict)
+students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
+avg = dict()
+students = sorted(students)
+print(students)
+a = sum(grades[0])/len(grades[0])
+a_1 = sum(grades[1])/len(grades[1])
+a_2 = sum(grades[2])/len(grades[2])
+a_3 = sum(grades[3])/len(grades[3])
+a_4 = sum(grades[4])/len(grades[4])
+print(a,a_1,a_2,a_3,a_4)
+avg[students[0]] = a
+avg[students[1]] = a_1
+avg[students[2]] = a_2
+avg[students[3]] = a_3
+avg[students[4]] = a_4
+print(avg)
+
+
