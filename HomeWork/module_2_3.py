e4bf040dc3b6129f9b2d08b2be92c325dc058598
Руслан <rustikbogdanov19@gmail.com>
2024-09-24 11:40:15 +0300

    24.09

---
+++ HomeWork/module_2_3.py
@@ -0,0 +1,9 @@
+numbers = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = 0
+
+while index < len(numbers):
+    if numbers[index] < 0:
+        break
+    print(numbers[index])
+    index += 1
